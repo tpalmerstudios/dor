32fed0d6a2c8719d4060117e9284c7311f811297
Tim Palmer <tpalmerstudios@gmail.com>
2020-03-31 08:26:21 -0500

    working with people.h

---
+++ storage/people.h
@@ -0,0 +1,29 @@
+#include <string>
+#include <iostream>
+#ifndef PEOPLE_H
+#define PEOPLE_H
+
+class People
+{
+private:
+		// Birth Detais
+		int dob, dod, myID;
+		// Physical desciption
+		int height, weight;
+		char hairColor, eyeColor;
+		string hairNotes; // Cut and dye
+		// Relationships
+		int humanID [5];
+		string humanNote [5];
+		// Possessions
+		int carID;
+		int homeID;
+		// Documents
+		// Pictures
+		// Work & Income
+
+public:
+		// accessor templates
+};
+#endif
+
